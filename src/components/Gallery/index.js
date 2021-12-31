99a140b24b642f25d70d3c3abcda6f3a019ed469
Dan <dan.pandolfo@gmail.com>
2021-12-31 18:04:43 -0500

    created photo list and gallery components

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import Photolist from '../PhotoList';
+
+function Gallery({currentCategory}) {
+    const { name, description } = currentCategory;
+
+    return (
+        <section>
+            <h1 data-testid='h1tag'>{capitalizeFirstLetter(name)}</h1>
+            <p>{description}</p>
+            <Photolist category={currentCategory.name} />
+        </section>
+    );
+}
+
+export default Gallery;
